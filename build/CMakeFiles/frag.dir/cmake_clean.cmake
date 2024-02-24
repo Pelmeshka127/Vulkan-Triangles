917bffc831e30f4a7e3098e9c7abca7e683748ba
Kirill Pavlovskiy <kirya0703@gmail.com>
2024-02-24 13:13:43 +0300

    Deleting build directory

--- build/CMakeFiles/frag.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/frag"
-  "frag.spv"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/frag.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
