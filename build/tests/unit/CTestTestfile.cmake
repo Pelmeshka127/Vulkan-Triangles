00281e704bd95fd6ffa94263c5f90b7d1c9c8822
Kirill Pavlovskiy <kirya0703@gmail.com>
2024-03-09 00:30:45 +0300

    trying to fix segfault

--- build/tests/unit/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/pelmeshka127/Desktop/ILab/Vulkan-Triangles/tests/unit
-# Build directory: /Users/pelmeshka127/Desktop/ILab/Vulkan-Triangles/build/tests/unit
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
